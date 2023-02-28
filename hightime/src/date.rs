aa81a5ecf09d64515a83d6ad2cdae1b8f3fb41e3
Nicholas Sim <nsim@posteo.net>
2023-02-28 19:47:26 +0800

    doc for initial items

--- hightime/src/date.rs
+++ hightime/src/date.rs
@@ -1,12 +1,26 @@
 use crate::Error;
 
+/// A date without time zone information.
 pub struct NaiveDate {}
 
 impl NaiveDate {
-    pub fn from_ymd(_year: i32, _month: i32, _day: i32) -> Result<Self, Error> {
+    /// Returns a [NaiveDate] from the calendar date.
+    ///
+    /// `year` is the Gregorian ordinal year.
+    ///
+    /// `month` is a number from 1 to 12.
+    ///
+    /// `day` is a number from 1 to 31.
+    ///
+    /// # Errors
+    ///
+    /// An error is returned if the date does not exists, e.g. April 31st of a given year.
+    // TODO: which error variant?
+    pub fn from_ymd(_year: i32, _month: u32, _day: u32) -> Result<Self, Error> {
         todo!()
     }
 
+    /// Returns a [NaiveDate] from the current system time.
     #[cfg(feature = "std")]
     pub fn today() -> Self {
         todo!()
--- hightime/src/error.rs
+++ hightime/src/error.rs
@@ -1,1 +1,3 @@
+/// An error returned by `hightime`.
+#[non_exhaustive]
 pub struct Error {}
